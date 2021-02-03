82e3dcbe3907721c4a6da9261349b50b7b881aba
iván.villagra <56157806+ivanVillagralatorre@users.noreply.github.com>
2021-02-03 20:51:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ function connect(){
     
 
     try{
-        $db = new PDO("mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_d6be82b678b6e84",'bc2d8f1edc0fa4','fbfee04e');
+        $db = new PDO("mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_12f326f918fecfd",'b677726516bfe2','f702ddf6');
         echo "conexión correcta a la base de datos";
         return $db;
     }
